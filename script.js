e5b1dd1616817249c8852154e2457d423a7ef914
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-07 16:24:16 +0500

    Styled Table

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-document.querySelector("#home > div > div:nth-child(1)").addEventListener("contextmenu", (e) => console.log(e));
-let section = document.querySelectorAll("section");
+document.querySelector("#home > div > div").addEventListener("contextmenu", (e) => console.log(e));
+
+let sections = document.querySelectorAll("section");
+let lists = [];
+
+window.onload = () => {
+	if (localStorage.getItem("lists") == null) {
+		localStorage.setItem("lists", null);
+	}
+};
+
+let createListBtn = document.querySelector(".create-list");
+createListBtn.addEventListener("click", () => {
+	sections[2].classList.remove("show");
+	sections[0].classList.add("show");
+});
+
+let closeBtns = document.querySelectorAll(".close");
+closeBtns.forEach((btn, index) => {
+	btn.addEventListener("click", () => {
+		sections[index].classList.remove("show");
+		sections[2].classList.add("show");
+	});
+});
+let approveList = document.querySelector(".check");
+approveList.addEventListener("click", () => {
+	sections[0].classList.remove("show");
+	sections[2].classList.add("show");
+});
